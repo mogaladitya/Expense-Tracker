3cb06ea8bb3b0709e14845953137ee666a66b015
adityamogal <adityamogal0001@gamil.com>
2025-07-28 10:15:08 +0530

    28 july

--- src/pages/Dashboard/Home.jsx
+++ src/pages/Dashboard/Home.jsx
@@ -12,6 +12,8 @@ import RecentTransactions from "../../components/Dashboard/RecentTransactions";
 import FinanceOverview from "../../components/Dashboard/FinanceOverview";
 import ExpenseTransactions from "../../components/Dashboard/ExpenseTransactions";
 import Last30DaysExpenses from "../../components/Dashboard/Last30DaysExpenses";
+import RecentIncomeWithChart from "../../components/Dashboard/RecentIncomeWithChart";
+import RecentIncome from "../../components/Dashboard/RecentIncome";
 
 const Home = () => {
 
@@ -45,6 +47,7 @@ const Home = () => {
     fetchDashboardData(); 
     return () => {};
   }, [])
+
 
   return (
     <DashboardLayout activeMenu="Dashboard">
@@ -91,6 +94,16 @@ const Home = () => {
 
         <Last30DaysExpenses
           data={dashboardData?.last30DaysExpenses?.transactions || []}
+        />
+
+        <RecentIncomeWithChart
+          data={dashboardData?.last60DaysIncome?.transactions?.slice(0, 4) || []}
+          totalIncome={dashboardData?.totalIncome || 0}
+        />
+
+        <RecentIncome
+          transactions={dashboardData?.last6DaysIncome?.transactions || []}
+          onSeeMore={() => navigate("/income")}
         />
       </div>
 
